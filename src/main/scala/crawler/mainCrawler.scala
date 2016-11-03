cc5c893c347c9b2991b53066ff64ca78d5d39204
Nguyen Ho Duy Tri <duytri2712@gmail.com>
2016-11-04 00:24:13 +0700

    Initialize

---
+++ src/main/scala/crawler/mainCrawler.scala
@@ -0,0 +1,9 @@
+package main.scala.crawler
+
+object mainCrawler {
+  def main(args: Array[String]): Unit = {
+    val startURL = ""
+    val delayInterval = 2000
+    println("Hello world!")
+  }
+}
